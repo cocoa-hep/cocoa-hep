0fe5c6703a6be55c4804e1d3cdf415688ee82292
Lorenzo Santi <l.santi@cern.ch>
2022-12-21 09:40:33 +0100

    modify pflow, add subtraction also for el, mu

--- SCD/src/Particle_flow_func.cc
+++ SCD/src/Particle_flow_func.cc
@@ -31,7 +31,7 @@ Particle_flow_func::Particle_flow_func(std::vector<Track_struct> &track_list, st
     
     for (int itrack = 0; itrack < size_track_list; itrack++)
     {
-        if (((abs(track_list.at(itrack).pdgcode) != 11) || (abs(track_list.at(itrack).pdgcode) != 13) )&& track_list.at(itrack).Is_Track_Useable())
+        if (track_list.at(itrack).Is_Track_Useable())
         {
             track_match_to_topoclusters(track_list.at(itrack));
             if (track_list.at(itrack).Rprime_to_closest_topoclusters.at(0) < delta_Rprime_threshold)
@@ -53,7 +53,7 @@ Particle_flow_func::Particle_flow_func(std::vector<Track_struct> &track_list, st
 
     for (int itrack = 0; itrack < size_track_list; itrack++)
     {
-        if (abs(track_list.at(itrack).pdgcode) != 11 && track_list.at(itrack).Is_Track_Useable())
+        if (track_list.at(itrack).Is_Track_Useable())
             pflow_list.emplace_back(track_list.at(itrack));
     }    
 }
