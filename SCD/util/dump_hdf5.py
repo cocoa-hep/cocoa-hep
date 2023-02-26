a4925b30a7053d4df75abd7e4e1ca527c5490a38
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2023-02-26 15:52:30 +0200

    added light option for omitting branches storing 2D array

--- SCD/util/dump_hdf5.py
+++ SCD/util/dump_hdf5.py
@@ -4,7 +4,7 @@ import uproot
 import numpy as np
 import h5py
 
-def reader(input_path, nevents=-1, firstevent=0, tree_name=""):
+def reader(input_path, nevents=-1, firstevent=0, tree_name="", light=False):
     
     rootfile = uproot.open(input_path)
     if len(rootfile.keys()) == 0:
@@ -21,13 +21,18 @@ def reader(input_path, nevents=-1, firstevent=0, tree_name=""):
     data_array = {}
 
     for branch_name in tree.keys():
+
         arr = tree[branch_name].array(library="np", entry_stop=lastevent,entry_start=firstevent)
+
         if isinstance(arr[0],uproot.STLVector):
 
-            arr = np.array([ #events
-                        np.array([ #rows
-                            np.array( #columns
-                                np.array(row.tolist() if len(row)!=0 else [np.NaN])
+            if light: #skip branches containing 2D arrays
+                continue
+
+            arr = np.array([ #branch
+                        np.array([ #event
+                            np.array( #row
+                                np.array(row.tolist() if len(row)!=0 else [np.NaN]) #column
                             )
                             for row in event
                         ],dtype=object)
@@ -56,6 +61,7 @@ def pad_array(arr,_max_length = (-1,-1)):
             )
             for idx, event in enumerate(arr)
         ]).astype('float32')
+
     else:
         pad_with = np.NaN if arr[0].dtype.kind == 'f' else -9999
         lengths = [len(event) for event in arr]
@@ -86,11 +92,11 @@ def writer(output_path,data_array,save_jagged=True):
         if save_jagged and branch_array[0].dtype!=object:
             shape = (num_entries, )
             dt = h5py.vlen_dtype(np.dtype('float32'))
+
         else:
             branch_array = pad_array(branch_array)
             shape = branch_array.shape
             dt = type(branch_array[0][0][0])
-
 
         dataset = file.create_dataset(
             branch_name,
@@ -115,12 +121,13 @@ def main():
     parser.add_argument("-n","--nevents", dest="nevents",       type=int,  help="number of events to parse", default=-1)
     parser.add_argument("-s","--start",   dest="start",         type=int,  help="event to start on", default=0)
     parser.add_argument("-j","--jagged",  dest="save_jagged",   type=int,  help="save output as jagged array (alternative is padded array)", default=1)
+    parser.add_argument("-l","--light",   dest="light",         type=int,  help="do not save expensive branches containing 2D arrays", default=0)
     args = parser.parse_args()
 
     if args.output == "":
         args.output = args.input.replace(".root",".h5")
 
-    data_array = reader(args.input, args.nevents, args.start, args.tree)
+    data_array = reader(args.input, args.nevents, args.start, args.tree, bool(args.light))
     writer(args.output,data_array, bool(args.save_jagged))
 
 if __name__ == '__main__':
