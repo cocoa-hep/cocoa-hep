6f695f00de514d7df2175dc0efd3f5c0b244a665
Patrick Rieck <patrick.rieck@cern.ch>
2024-04-16 11:50:36 -0500

    Pythia8 HepMC3 plugin taken from pythia (no plugin shipped with HepMC3 anymore). Some remarks on installation in the committed setup file.

--- setup_cvmfs_hepmc3.sh
+++ setup_cvmfs_hepmc3.sh
@@ -6,8 +6,12 @@ source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/ROOT/6.18.04/x86_64-centos7-gcc8-
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/jsoncpp/1.8.4/x86_64-centos7-gcc8-opt/jsoncpp-env.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/CMake/3.14.3/x86_64-centos7-gcc8-opt/CMake-env.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/cmaketools/1.8/x86_64-centos7-gcc8-opt/cmaketools-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/pythia8/301/x86_64-centos7-gcc8-opt/pythia8env-genser.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/fastjet/3.3.2/x86_64-centos7-gcc8-opt/fastjet-env.sh
-export HEPMC_HOME=/home/pr2329/cocoa-hep/hepmc3
+#source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/pythia8/301/x86_64-centos7-gcc8-opt/pythia8env-genser.sh
+# < install hepmc3 if not done yet >
+export HEPMC_HOME=/home/pr2329/cocoa-hep/hepmc3_noPythia8Interface
+ln -s ${HEPMC_HOME}/lib64 ${HEPMC_HOME}/lib
+# < install pythia 8, using './configure --with-hepmc3=${HEPMC_HOME}' >
+export PYTHIA8_HOME=/home/pr2329/pythia8311
 export PYTHIA8DATA=$PYTHIA8_HOME/share/Pythia8/xmldoc   
 cd $CURRENTDIR
